289320611ceb6159a42a11677c6d94cb360b6fa0
NGUSA30 <csngusa06@gmail.com>
2025-06-01 21:05:17 +0300

    first commit

---
+++ src/main/java/com/example/automotive/AutomotiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.automotive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/automotive/AutomotiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automotive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomotiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
